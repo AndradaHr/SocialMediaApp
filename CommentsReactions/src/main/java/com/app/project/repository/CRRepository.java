0d6fc194e2bf8bba0be78a85b853ff3d0d3739e3
AndradaHr <hrubanandrada@gmail.com>
2024-01-03 13:53:11 +0200

    created functions for CommentsReactions Project

--- CommentsReactions/src/main/java/com/app/project/repository/CRRepository.java
+++
@@ -1,9 +0,0 @@
-package com.app.project.repository;
-
-import com.app.project.model.CR;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CRRepository extends JpaRepository<CR, Long> {
-}
